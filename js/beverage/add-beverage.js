747ff703f6a45e4355d57251bf24960dbb9150be
Sebas <sebas.lucero10@hotmail.com>
2022-10-17 11:51:59 -0500

    feat: beverage functionality added to frontend

---
+++ js/beverage/add-beverage.js
@@ -0,0 +1,48 @@
+function postBeverage(beverage) {
+    console.log(beverage)
+    fetch('http://127.0.0.1:5000/beverage/', {
+        method: 'POST',
+        body: JSON.stringify(beverage),
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+    })
+        .then(res => res.json())
+        .then(res => showNotification());
+
+
+}
+
+/**
+ * Get the form and submit it with fetch API
+ */
+let beverageForm = $("#beverage-form");
+beverageForm.submit(event => {
+
+    let beverage = getBeverageData();
+    postBeverage(beverage);
+
+    event.preventDefault();
+    event.currentTarget.reset();
+});
+
+/**
+ * Gets the order data with JQuery
+ */
+function getBeverageData() {
+
+    return {
+        name: $("input[name='name']").val(),
+        size: $("input[name='size']").val(),
+        price: $("input[name='price']").val(),
+    };
+}
+
+/**
+ * Shows a notification when the order is accepted
+ */
+function showNotification() {
+    let beverageAlert = $("#beverage-alert");
+    beverageAlert.toggle();
+    setTimeout(() => beverageAlert.toggle(), 5000);
+}
